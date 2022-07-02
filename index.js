d38c1dd3584863d784161e9404beac7c4f067d53
Muhammed Faheem <56709898+faheem-cmd@users.noreply.github.com>
2022-07-02 13:06:08 +0530

    bug fixes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function currencyFormater(num, symbol) {
-  return symbol + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+  let amount = parseFloat(num);
+  return symbol + amount.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
 }
 
 module.exports = currencyFormater;
